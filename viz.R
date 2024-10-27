1db62608140018b24eb513778c95f9f264e33a3e
Siyuan Feng <ucfnsfe@ucl.ac.uk>
2024-10-27 17:13:48 +0000

    change

--- viz.R
+++ viz.R
@@ -9,3 +9,15 @@ print('Hi there!')
 
 library(usethis)
 use_git()
+library(here)
+here::here()
+# 安装和加载 rstudioapi 包
+install.packages("rstudioapi") # 如果尚未安装
+library(rstudioapi)
+
+# 查看当前项目路径
+rstudioapi::getActiveProject()
+library(rstudioapi)
+rstudioapi::getActiveProject()
+
+print('hello world')
