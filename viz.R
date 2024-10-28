6647a3102649aceef1fe9d16b215012ff25379a4
Siyuan Feng <ucfnsfe@ucl.ac.uk>
2024-10-28 14:07:12 +0000

    gis_code

--- viz.R
+++ viz.R
@@ -1,23 +1,22 @@
-library(tidyverse)
-library(palmerpenguins)
+---
+editor_options: 
+  markdown: 
+    wrap: 72
+---
 
-penguins %>% 
-  ggplot(aes(x = bill_depth_mm)) + 
-  geom_histogram()
+library(tidyverse) library(palmerpenguins)
+
+penguins %\>% ggplot(aes(x = bill_depth_mm)) + geom_histogram()
 
 print('Hi there!')
 
-library(usethis)
-use_git()
-library(here)
-here::here()
-# 安装和加载 rstudioapi 包
-install.packages("rstudioapi") # 如果尚未安装
+library(usethis) use_git() library(here) here::here() \# 安装和加载
+rstudioapi 包 install.packages("rstudioapi") \# 如果尚未安装
 library(rstudioapi)
 
 # 查看当前项目路径
-rstudioapi::getActiveProject()
-library(rstudioapi)
+
+rstudioapi::getActiveProject() library(rstudioapi)
 rstudioapi::getActiveProject()
 
 print('hello world')
